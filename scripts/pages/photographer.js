ab51b35cbe9eff80f2234baa383f7134b2d31cdf
David <david.alvesdebrito@gmail.com>
2022-01-19 15:44:37 +0100

    Ajout fonction getPhotographersId

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,18 +1,19 @@
 //Mettre le code JavaScript lié à la page photographer.html
-async function getPhotographerById(id) {
-    const id = await fetch("data/photographers.json") 
-    
+
+async function getPhotographerById(){
+    const photographers = await fetch ("data/photographers.json") 
+        .then(result => result.json()) 
+        .then(photographers => photographers.id); {
+            for (let id of photographers){
+                console.log(photographers.id)
+                return ({...id});
+            }
+            //faire ma boucle ici
+        };
 }
 
-function fetchDataIds(){
-    fetch("data/photographers.json")
-        .then(result => result.json()) 
-        .then(photographers =>{
-            //faire ma boucle ici
-        });
-} 
+getPhotographerById();
 
-fetchDataIds();
 
 let params = new URLSearchParams(document.location.search);
 console.log(params);
